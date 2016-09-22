f77fdae3e09d5a504b03a29fd4cb89391b1832c4
Dipika Pandey <dipika.pandey@wct-inc.com>
2016-09-22 02:24:38 -0400

    Committing output init qs

---
+++ outputInit/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class string{
+
+public:
+    string() {std::cout << "default" << std::endl;}
+    string(const char*) {std::cout << "convert" << std::endl;}
+    string(const string&) {std::cout << "copy" << std::endl;}
+    ~string() {std::cout << "destructor" << std::endl;}
+    
+    string& operator=(const string& rhs){
+        std::cout << "assign" << std::endl;
+        return *this;
+    }
+};
+
+class student{
+
+public:
+    student(const char* n){
+        name = n;
+    }
+    
+private:
+    string name;
+};
+
+int main(){
+    student s("Dipika");
+}
