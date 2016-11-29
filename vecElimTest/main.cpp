00e26a8d0b65b1e502ce5dd1c209df5e96ad92e1
Dipika Pandey <dipika.pandey@wct-inc.com>
2016-11-28 19:36:47 -0500

    Committing previous exams paper

--- vecElimTest/main.cpp
+++ vecElimTest/main.cpp
@@ -3,31 +3,32 @@
 #include <vector>
 using std::cout; using std::endl; using std::vector;
 const unsigned int MAX = 10;
+
 void initialize(vector<int>& vec) {
-for ( unsigned int i = 0; i < MAX; ++i ) {
-vec.push_back( rand() % 100 );
-}
+    for ( unsigned int i = 0; i < MAX; ++i ) {
+        vec.push_back( rand() % 100 );
+    }
 }
 
 
 void eliminate_all(vector<int>& vec, int n);
 
 int main() {
-vector<int> vec;
-vec.reserve(MAX);
-initialize(vec);
-vec.push_back(35);
-//print(vec);
-eliminate_all(vec, 35);
+    vector<int> vec;
+    vec.reserve(MAX);
+    initialize(vec);
+    vec.push_back(35);
+    //print(vec);
+    eliminate_all(vec, 35);
 
 }
 
 void eliminate_all(vector<int>& vec, int n) {
-vector<int>::iterator ptr = vec.begin();
-while ( ptr != vec.end() ) {
-if ( *ptr == n ) {
-ptr = vec.erase(ptr);
-}
-else ++ptr;
-}
+    vector<int>::iterator ptr = vec.begin();
+    while ( ptr != vec.end() ) {
+        if ( *ptr == n ) {
+            ptr = vec.erase(ptr);
+        }
+        else ++ptr;
+    }
 }
